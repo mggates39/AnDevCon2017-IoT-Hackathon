db6b4d64a07f159362d845776200ecaa28dd9548
Marshall G Gates <mggates@gmail.com>
2017-07-19 15:46:49 -0400

    Attempted to add new message page

---
+++ Test01/mobile/src/main/java/com/corgihat/iot/test01/NewMessageActivity.java
@@ -0,0 +1,13 @@
+package com.corgihat.iot.test01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_message);
+    }
+}
